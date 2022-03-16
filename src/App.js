69b47564a59d747d1205c16310ede7aba621e58a
Alicja Stelmach <alicjaastelmach@gmail.com>
2022-03-16 12:21:38 +0100

    sort notifications by date, from newest to oldest

--- src/App.js
+++ src/App.js
@@ -10,17 +10,19 @@ const App = () => {
       <div className='header__container'>
         <header>Notifications</header>
       </div>
-      <div className='notification__contianer'>
-        {content.map((item) => (
-          <NotificationItem
-            key={item.id}
-            status={item.status}
-            date={moment(item.date).format('DD.MM.YYYY')}
-            title={item.title}
-            description={item.desc}
-            id={item.id}
-          />
-        ))}
+      <div className='notification__container'>
+        {content
+          .sort((a, b) => (moment(a.date).isBefore(moment(b.date)) ? 1 : -1))
+          .map((item) => (
+            <NotificationItem
+              key={item.id}
+              status={item.status}
+              date={moment(item.date).format('DD.MM.YYYY')}
+              title={item.title}
+              description={item.desc}
+              id={item.id}
+            />
+          ))}
       </div>
     </main>
   );
