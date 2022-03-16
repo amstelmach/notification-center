0c90944e33ab94df3f8b9740f4b5c593abfdeb87
Alicja Stelmach <alicjaastelmach@gmail.com>
2022-03-16 12:02:43 +0100

    display all notifications

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-function App() {
-  return <div>app</div>;
-}
+import React, { useState } from 'react';
+import { notifications } from './utils/content';
+import NotificationItem from './NotificationItem';
+
+const App = () => {
+  const [content, setContent] = useState(notifications);
+  return (
+    <main>
+      <div className='header__container'>
+        <header>Notifications</header>
+      </div>
+      <div className='notification__contianer'>
+        {content.map((item) => (
+          <NotificationItem
+            key={item.id}
+            status={item.status}
+            date={item.date}
+            title={item.title}
+            description={item.desc}
+            id={item.id}
+          />
+        ))}
+      </div>
+    </main>
+  );
+};
 
 export default App;
---
+++ src/NotificationItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './notificationitem.css';
+
+const NotificationItem = ({ status, date, title, description }) => {
+  return (
+    <div className='wrapper'>
+      <div className='accordion__item'>
+        <div className='accordion__header'>
+          <div className='accordion__status'>{status}</div>
+          <div className='accordion__date'>{date}</div>
+          <div className='accordion__title'>{title}</div>
+          <div>arrow</div>
+        </div>
+        <div className='accordion__content'>
+          <button className='button'>delete</button>
+          <div className='accordion__content--desc'>{description}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotificationItem;
