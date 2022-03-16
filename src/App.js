c1708d493e44549a241991abfaf8f7dbc2b92048
Alicja Stelmach <alicjaastelmach@gmail.com>
2022-03-16 13:38:34 +0100

    add NavBar component to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { notifications } from './utils/content';
 import NotificationItem from './NotificationItem';
 import moment from 'moment';
 import Information from './Information';
+import NavBar from './NavBar';
 
 const App = () => {
   const [content, setContent] = useState(notifications);
@@ -14,27 +15,30 @@ const App = () => {
   };
 
   return (
-    <main>
-      <div className='header__container'>
-        <header>Notifications</header>
-      </div>
-      <div className='notification__container'>
-        {content
-          .sort((a, b) => (moment(a.date).isBefore(moment(b.date)) ? 1 : -1))
-          .map((item) => (
-            <NotificationItem
-              key={item.id}
-              status={item.status}
-              date={moment(item.date).format('DD.MM.YYYY')}
-              title={item.title}
-              description={item.desc}
-              id={item.id}
-              onClick={() => handleRemove(item.id)}
-            />
-          ))}
-      </div>
-      <div>{content.length === 0 ? <Information /> : null}</div>
-    </main>
+    <>
+      <NavBar />
+      <main>
+        <div className='header__container'>
+          <header>Notifications</header>
+        </div>
+        <div className='notification__container'>
+          {content
+            .sort((a, b) => (moment(a.date).isBefore(moment(b.date)) ? 1 : -1))
+            .map((item) => (
+              <NotificationItem
+                key={item.id}
+                status={item.status}
+                date={moment(item.date).format('DD.MM.YYYY')}
+                title={item.title}
+                description={item.desc}
+                id={item.id}
+                onClick={() => handleRemove(item.id)}
+              />
+            ))}
+        </div>
+        <div>{content.length === 0 ? <Information /> : null}</div>
+      </main>
+    </>
   );
 };
 
