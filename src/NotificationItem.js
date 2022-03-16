198300325c0d1e159139e04a3cf15dd78ea828a5
Alicja Stelmach <alicjaastelmach@gmail.com>
2022-03-16 12:07:07 +0100

    add state to toggle accordion with single notification

--- src/NotificationItem.js
+++ src/NotificationItem.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './notificationitem.css';
 
 const NotificationItem = ({ status, date, title, description }) => {
+  const [isActive, setIsActive] = useState(false);
   return (
     <div className='wrapper'>
       <div className='accordion__item'>
-        <div className='accordion__header'>
+        <div
+          className='accordion__header'
+          onClick={() => setIsActive(!isActive)}
+        >
           <div className='accordion__status'>{status}</div>
           <div className='accordion__date'>{date}</div>
           <div className='accordion__title'>{title}</div>
           <div>arrow</div>
         </div>
-        <div className='accordion__content'>
-          <button className='button'>delete</button>
-          <div className='accordion__content--desc'>{description}</div>
-        </div>
+
+        {isActive && (
+          <div className='accordion__content'>
+            <button className='button'>delete</button>
+            <div className='accordion__content--desc'>{description}</div>
+          </div>
+        )}
       </div>
     </div>
   );
