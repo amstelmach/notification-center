4c3f8b4e912ec10e09704257447a6136cbcfebbd
Alicja <alicjaprivate@Alicjas-MacBook-Pro.local>
2022-09-19 14:09:50 +0200

    responsive layout improvement

--- src/NotificationItem.js
+++ src/NotificationItem.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './notificationitem.css';
 import ChevronDown from './utils/ChevronDown';
 import ChevronUp from './utils/ChevronUp';
-import StatusDot from './utils/StatusDot';
 
 const NotificationItem = ({ status, date, title, description, onClick }) => {
   const [isActive, setIsActive] = useState(false);
@@ -13,11 +12,6 @@ const NotificationItem = ({ status, date, title, description, onClick }) => {
           className='accordion__header'
           onClick={() => setIsActive(!isActive)}
         >
-          <div className='accordion__status'>
-            {status === 'new' ? (
-              <StatusDot height={15} width={25} fill={'#daecf0'} />
-            ) : null}
-          </div>
           <div className='accordion__date'>{date}</div>
           <div className='accordion__title'>{title}</div>
           {isActive ? (
